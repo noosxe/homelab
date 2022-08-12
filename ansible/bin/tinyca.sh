80f48bd66fa5dcdcac43f22467752e63e6783160
Levon Kirakosyan <noosx.e@gmail.com>
2022-08-11 17:46:44 -0700

    Add tinyca.sh binary for easier playbook execution.

---
+++ ansible/bin/tinyca.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+optstring="f"
+
+f=0
+
+while getopts ${optstring} arg; do
+  case "${arg}" in
+    f)
+      f=1
+      ;;
+  esac
+done
+
+if [ $f -eq 1 ]; then
+  ansible-playbook playbooks/tinyca.yaml -K --extra-vars "tinyca_full=yes"
+else
+  ansible-playbook playbooks/tinyca.yaml
+fi
