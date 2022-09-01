8f71c6fa6352fb18e645ab9f9caff9e395b8d4ca
Levon Kirakosyan <noosx.e@gmail.com>
2022-09-01 16:42:26 -0700

    Add time machine vm deploy script.

---
+++ ansible/bin/time_machine.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+optstring="f"
+
+f=0
+
+while getopts ${optstring} arg; do
+  case "${arg}" in
+    f)
+      f=1
+      ;;
+  esac
+done
+
+if [ $f -eq 1 ]; then
+  ansible-playbook playbooks/time_machine.yaml -K --extra-vars "time_machine_full=yes"
+else
+  ansible-playbook playbooks/time_machine.yaml
+fi
